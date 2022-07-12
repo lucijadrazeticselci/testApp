4e722cb4b3477fb2800e6c6ea24f27d508921b6c
juhaszjosip <juhasz.josip@gmail.com>
2022-07-12 15:21:48 +0200

    Initial commit

---
+++ testApp/testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Josip Juhasz on 12.07.2022..
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
